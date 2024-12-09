436127a99d389117bc5c442e9e112e544db9f95a
Galers <galosaandrew@gmail.com>
2024-12-09 20:22:04 +0200

    test: Add test

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,45 @@
-'use strict';
+"use strict";
 
-describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+describe("restoreNames", () => {
+  const { restoreNames } = require("./restoreNames");
 
-  it('should ', () => {
+  it(`should restore firstName
+      from fullName when firstName is undefined`, () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: "Holy",
+        fullName: "Jack Holy",
+      },
+      {
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
+    ];
 
+    restoreNames(users);
+
+    expect(users[0].firstName).toEqual("Jack");
+    expect(users[1].firstName).toEqual("Mike");
   });
 
-  // write tests here
+  it(`should not change firstName if it is already defined`, () => {
+    const users = [
+      {
+        firstName: "Jack",
+        lastName: "Holy",
+        fullName: "Jack Holy",
+      },
+      {
+        firstName: "Mike",
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toEqual("Jack");
+    expect(users[1].firstName).toEqual("Mike");
+  });
 });
